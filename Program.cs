223bd655c514b3f9fe5c253caf98cb312bdeea0e
Jeffry1990 <109137757+Jeffry1990@users.noreply.github.com>
2024-12-11 12:57:44 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -6,16 +6,16 @@
         {
             int randomMinNumber = 1;
             int randomMaxNumber = 101;
-            int specifiedNumber1 = 3;
-            int specifiedNumber2 = 5;
+            int firstDivider1 = 3;
+            int firstDivider2 = 5;
             Random random = new Random();
             int number = random.Next(randomMinNumber,randomMaxNumber);
 
             Console.WriteLine("Случайное число: " + number);
 
-            for (int i = specifiedNumber1; i <= number; i++)
+            for (int i = firstDivider1; i <= number; i++)        
             {
-                if (i % specifiedNumber1 == 0 || i % specifiedNumber2 == 0)
+                if (i % firstDivider1 == 0 || i % firstDivider2 == 0)
                 {
                     Console.WriteLine(i);
                 }
