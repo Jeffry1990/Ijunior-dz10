884d0cea162cf1dd72478b728c589e1537f7ff4e
Jeffry1990 <warlock_artur@mail.ru>
2024-12-11 07:19:14 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp50
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int randomMinNumber = 1;
+            int randomMaxNumber = 101;
+            int specifiedNumber1 = 3;
+            int specifiedNumber2 = 5;
+            Random random = new Random();
+            int number = random.Next(randomMinNumber,randomMaxNumber);
+
+            Console.WriteLine("Случайное число: " + number);
+
+            for (int i = specifiedNumber1; i <= number; i++)
+            {
+                if (i % specifiedNumber1 == 0 || i % specifiedNumber2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
